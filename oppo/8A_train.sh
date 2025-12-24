29c73f30331c3378f2934fea4118b18edb4eeb57
yyy195 <YUYUNYAO2002@126.com>
2025-12-24 20:36:23 +0800

    ablation-nonoise

--- oppo/8A_train.sh
+++ oppo/8A_train.sh
@@ -47,8 +47,8 @@ python3 -m verl.trainer.main_ppo \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
-    trainer.project_name='yyy-8A800_CUT_Batch_Qwen25-new' \
-    trainer.experiment_name='yyy-Training_CUT_Qwen25_3b-new' \
+    trainer.project_name='yyy-nonoise-ablation' \
+    trainer.experiment_name='yyy-nonoise-ablation' \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=20 \
